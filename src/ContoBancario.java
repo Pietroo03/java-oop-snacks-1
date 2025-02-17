284616bcfd442e36dbff362fcf02c0776063de3d
Pietroo03 <pietro.ponte.03@gmail.com>
2025-02-17 16:44:36 +0100

    add class for second snack

---
+++ src/ContoBancario.java
@@ -0,0 +1,7 @@
+// Crea una classe ContoBancario con attributi per numero di conto e saldo. 
+// Implementa un costruttore che accetta il numero di conto e inizializza il saldo a zero. 
+// Aggiungi metodi pubblici per depositare denaro sul conto, prelevare denaro dal conto e ottenere il saldo corrente.
+
+public class ContoBancario {
+
+}
