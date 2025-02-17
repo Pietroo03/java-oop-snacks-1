3480f0f7e83b513e06351f814c0f8aaecc094968
Pietroo03 <pietro.ponte.03@gmail.com>
2025-02-17 17:04:36 +0100

    complete snack 2

--- src/ContoBancario.java
+++ src/ContoBancario.java
@@ -2,6 +2,27 @@
 // Implementa un costruttore che accetta il numero di conto e inizializza il saldo a zero. 
 // Aggiungi metodi pubblici per depositare denaro sul conto, prelevare denaro dal conto e ottenere il saldo corrente.
 
+import java.util.Random;
+
 public class ContoBancario {
+
+    private int numeroConto;
+    private float saldo;
+
+    public ContoBancario() {
+        Random rand = new Random();
+        this.numeroConto = rand.nextInt(999999);
+        this.saldo = 0;
+    }
+
+    public float depositaDenaro(float denaroAggiunto) {
+        this.saldo += denaroAggiunto;
+        return this.saldo;
+    }
+
+    public float prelevaDenaro(float denaroPrelevato) {
+        this.saldo -= denaroPrelevato;
+        return this.saldo;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,12 @@ public class Main {
         Studente studente1 = new Studente("Pietro", "Ponte", 22);
 
         System.out.println(studente1.datiConcatenati());
+
+        ContoBancario conto1 = new ContoBancario();
+
+        System.out.println(conto1.depositaDenaro(325.00f));
+        System.out.println(conto1.depositaDenaro(250.00f));
+        System.out.println(conto1.depositaDenaro(723.50f));
+        System.out.println(conto1.prelevaDenaro(150.00f));
     }
 }
